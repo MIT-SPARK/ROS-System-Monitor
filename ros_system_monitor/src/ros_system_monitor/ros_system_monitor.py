aa565153af5e8787cd09d5332747f2a84e79e911
Nathan Hughes <nathan.h.hughes@gmail.com>
2025-03-06 14:14:16 +0000

    run ruff

--- ros_system_monitor/src/ros_system_monitor/ros_system_monitor.py
+++ ros_system_monitor/src/ros_system_monitor/ros_system_monitor.py
@@ -19,14 +19,17 @@ import argparse
 
 CLEAN_TABLE_PRINTS = True
 
+
 @dataclass
 class TrackedNodeInfo:
     node_info: NodeInfo
     last_heartbeat: int
 
+
 @dataclass
 class DiagnosticTable:
     rows: Dict[str, TrackedNodeInfo]
+
 
 def info_to_row(info: TrackedNodeInfo):
     c1 = info.node_info.nickname
@@ -35,7 +38,7 @@ def info_to_row(info: TrackedNodeInfo):
     c4 = info.node_info.notes
 
     t_now = time.time()
-    dt = (t_now - info.last_heartbeat / 1e9)
+    dt = t_now - info.last_heartbeat / 1e9
     c5 = dt
 
     max_heartbeat_interval = 10
@@ -43,19 +46,19 @@ def info_to_row(info: TrackedNodeInfo):
     if dt > max_heartbeat_interval:
         status = Status.NO_HB
 
-    status_to_color = {Status.NOMINAL: "green",
-                       Status.WARNING: "yellow",
-                       Status.ERROR: "red",
-                       Status.NO_HB: "yellow",
-                       Status.STARTUP: "yellow",
+    status_to_color = {
+        Status.NOMINAL: "green",
+        Status.WARNING: "yellow",
+        Status.ERROR: "red",
+        Status.NO_HB: "yellow",
+        Status.STARTUP: "yellow",
     }
-                       
+
     color = status_to_color[status]
 
     row = (c1, c2, c3, c4, c5)
     return tuple(map(lambda x: f"[{color}]{x}[/{color}]", row))
 
-    
 
 def generate_table(diagnostics: DiagnosticTable):
     table = Table(show_header=True, header_style="bold magenta")
@@ -71,22 +74,24 @@ def generate_table(diagnostics: DiagnosticTable):
         table.add_row(*info_to_row(row))
     return table
 
+
 def print_table(console, table: Table):
     if CLEAN_TABLE_PRINTS:
         sys.stdout.write(chr(27) + "[2J")
         sys.stdout.write("\033[0;0H")
     console.print(table)
-    
+
 
 @dataclass
 class SystemMonitorConfig(Config):
     nodes_to_track: List[str] = field(default_factory=lambda: [])
     external_monitors: Any = config_field("external_monitor")
-    #external_monitors: list = field(default_factory=lambda: [])
+    # external_monitors: list = field(default_factory=lambda: [])
 
     @classmethod
     def load(cls, path: str):
         return Config.load(SystemMonitorConfig, path)
+
 
 class SystemMonitor(Node):
     def __init__(self, config_path):
@@ -97,18 +102,20 @@ class SystemMonitor(Node):
         self.external_info_subscribers = []
 
         discover_plugins("rsm_")
-        #plugins = discover_plugins("rsm_")
-        #self.get_logger().info(f"plugins: {plugins}")
+        # plugins = discover_plugins("rsm_")
+        # self.get_logger().info(f"plugins: {plugins}")
         self.config = SystemMonitorConfig.load(config_path)
 
         tracked_infos = {}
         for n in self.config.nodes_to_track:
-            info = NodeInfo(nickname=n, node_name="???", status=Status.NO_HB, notes="Not yet seen")
+            info = NodeInfo(
+                nickname=n, node_name="???", status=Status.NO_HB, notes="Not yet seen"
+            )
             tracked_info = TrackedNodeInfo(info, 0)
             tracked_infos[n] = tracked_info
         self.diagnostics = DiagnosticTable(tracked_infos)
 
-        #self.external_monitors = [m.create() for m in self.config.external_monitors]
+        # self.external_monitors = [m.create() for m in self.config.external_monitors]
         self.external_monitors = [self.config.external_monitors.create()]
         for e in self.external_monitors:
             e.register_callbacks(self)
@@ -122,29 +129,30 @@ class SystemMonitor(Node):
         self.i = 0
 
     def timer_callback(self):
-
         with self.info_lock:
             table = generate_table(self.diagnostics)
         print_table(self.console, table)
 
     def callback(self, msg):
-
         info = NodeInfo.from_ros(msg)
         self.update_node_info(info)
-       
+
     def add_subscribers(self, subscribers):
         self.external_info_subscribers += subscribers
 
     def update_node_info(self, info):
-        tracked_info = TrackedNodeInfo(info, time.time()*1e9)
+        tracked_info = TrackedNodeInfo(info, time.time() * 1e9)
 
         with self.info_lock:
             tracked_nodes = self.diagnostics.rows.keys()
             if info.nickname in tracked_nodes:
                 self.diagnostics.rows[info.nickname] = tracked_info
             else:
-                self.get_logger().error(f"Received diagnostics from untracked node: {info.nickname} ({info.node_name})")
- 
+                self.get_logger().error(
+                    f"Received diagnostics from untracked node: {info.nickname} ({info.node_name})"
+                )
+
+
 def main(args=None):
     rclpy.init(args=args)
     parser = argparse.ArgumentParser()
@@ -156,4 +164,3 @@ def main(args=None):
 
 if __name__ == "__main__":
     main()
-
